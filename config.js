19e7f4d57bde4904ec900ac0e28a85cfde9ad207
Thuongnin <86741533+Thuongnin@users.noreply.github.com>
2021-07-01 01:53:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 const CONFIG = {
     introTitle: 'Phương à!',
-    introDesc: `Bồ Công Anh bay khi có gió
+    introDesc: `
+Bồ Công Anh bay khi có gió
 T chỉ cười khi ở đó có Phương.
 Nhân gian vốn lắm bộn bề
-Sao không bỏ hết để về bên nhau`,
+Sao không bỏ hết để về bên nhau
+
+`,
     btnIntro: 'hihi',
     title: 'Phải chăng t đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng t đã say ngay từ lúc thấy nụ cười ấy ',
@@ -12,9 +15,8 @@ Sao không bỏ hết để về bên nhau`,
     question:'Trên thế giới hơn 7 tỉ người mà sao t lại yêu Phương<3',
     btnReply: 'Gửi cho t <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: ' đố ai quét sạch lá rừng
-đố ai khuyên được t ngừng yêu Phương',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 't biết mà 🥰. Yêu P nhiều nhiều 😘😘',
+    messDesc: 'Tối nay P ngủ ngon',
+    btnAccept: 'ngủ ngonnn <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
